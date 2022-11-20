c70566453647d4c94616664ac3a39ceef79d27b2
Beirao <thomasmarques911@gmail.com>
2022-11-19 23:04:33 -0300

    optimisation of the search engine

--- pages/blog.js
+++ pages/blog.js
@@ -34,7 +34,6 @@ export default function blog({ topics }) {
 
   useEffect(() => {
     setTopicTemp(search(topics, searchValue));
-    console.log("searchhh : ", searchValue);
   }, [searchValue]);
 
   return (
